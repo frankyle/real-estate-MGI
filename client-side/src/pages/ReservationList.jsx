de5976ce22b9489a188517979442753a174f15ba
frankyle <magaifrank@gmail.com>
2024-03-17 09:01:24 +0300

    Added functionalty of Reservation list, wishlist, and Property list

--- client-side/src/pages/ReservationList.jsx
+++ client-side/src/pages/ReservationList.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+// Importing necessary dependencies
+import { useEffect, useState } from "react"; // React hooks for managing component state and lifecycle
+import "../styles/List.scss"; // Importing stylesheet
+import Loader from "../components/Loader"; // Importing Loader component
+import Navbar from "../components/Navbar"; // Importing Navbar component
+import { useDispatch, useSelector } from "react-redux"; // Redux hooks for accessing Redux store
+import { setReservationList } from "../redux/state"; // Importing Redux action creator
+import ListingCard from "../components/ListingCard"; // Importing ListingCard component
+import Footer from "../components/Footer"; // Importing Footer component
 
-function ReservationList() {
-  return (
-    <div>ReservationList</div>
-  )
-}
+// ReservationList component definition
+const ReservationList = () => {
+  // State variable for loading indicator
+  const [loading, setLoading] = useState(true);
+  // Retrieving user ID from Redux store
+  const userId = useSelector((state) => state.user._id);
+  // Retrieving reservation list from Redux store
+  const reservationList = useSelector((state) => state.user.reservationList);
 
-export default ReservationList
+  // Redux dispatch hook
+  const dispatch = useDispatch();
+
+  // Function to fetch reservation list from the server
+  const getReservationList = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/users/${userId}/reservations`,
+        {
+          method: "GET",
+        }
+      );
+
+      const data = await response.json();
+      // Dispatching action to update reservation list in Redux store
+      dispatch(setReservationList(data));
+      setLoading(false); // Setting loading state to false after data is fetched
+    } catch (err) {
+      console.log("Fetch Reservation List failed!", err.message);
+    }
+  };
+
+  // useEffect hook to fetch reservation list when component mounts
+  useEffect(() => {
+    getReservationList(); // Calling getReservationList function
+  }, []);
+
+  // JSX content to be rendered
+  return loading ? ( // If loading state is true, render Loader component
+    <Loader />
+  ) : ( // If loading state is false, render reservation list
+    <>
+      <Navbar /> {/* Rendering Navbar component */}
+      <h1 className="title-list">Your Reservation List</h1> {/* Title for reservation list */}
+      <div className="list">
+        {reservationList?.map(({ listingId, hostId, startDate, endDate, totalPrice, booking=true }) => (
+          <ListingCard // Rendering ListingCard component for each reservation
+            key={listingId._id} // Key prop for optimization and unique identification
+            listingId={listingId._id} // Listing ID
+            creator={hostId._id} // Host ID
+            listingPhotoPaths={listingId.listingPhotoPaths} // Photo paths for listing
+            city={listingId.city} // City of listing
+            province={listingId.province} // Province of listing
+            country={listingId.country} // Country of listing
+            category={listingId.category} // Category of listing
+            startDate={startDate} // Start date of reservation
+            endDate={endDate} // End date of reservation
+            totalPrice={totalPrice} // Total price of reservation
+            booking={booking} // Booking status
+          />
+        ))}
+      </div>
+      <Footer /> {/* Rendering Footer component */}
+    </>
+  );
+};
+
+export default ReservationList; // Exporting ReservationList component
--- client-side/src/pages/SearchPage.jsx
+++ client-side/src/pages/SearchPage.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+// Importing necessary dependencies
+import { useParams } from "react-router-dom"; // React hook for accessing route parameters
+import "../styles/List.scss"; // Importing stylesheet
+import { useSelector, useDispatch } from "react-redux"; // Redux hooks for accessing Redux store and dispatching actions
+import { setListings } from "../redux/state"; // Importing Redux action creator
+import { useEffect, useState } from "react"; // React hooks for managing component state and lifecycle
+import Loader from "../components/Loader"; // Importing Loader component
+import Navbar from "../components/Navbar"; // Importing Navbar component
+import ListingCard from "../components/ListingCard"; // Importing ListingCard component
+import Footer from "../components/Footer"; // Importing Footer component
 
-function SearchPage() {
-  return (
-    <div>SearchPage</div>
-  )
-}
+// SearchPage component definition
+const SearchPage = () => {
+  // State variable for loading indicator
+  const [loading, setLoading] = useState(true);
+  // Retrieving search query from route parameters
+  const { search } = useParams();
+  // Retrieving listings from Redux store
+  const listings = useSelector((state) => state.listings);
+  // Redux dispatch hook
+  const dispatch = useDispatch();
 
-export default SearchPage
+  // Function to fetch search listings from the server
+  const getSearchListings = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/properties/search/${search}`, {
+        method: "GET"
+      });
+
+      const data = await response.json();
+      // Dispatching action to update listings in Redux store
+      dispatch(setListings({ listings: data }));
+      setLoading(false); // Setting loading state to false after data is fetched
+    } catch (err) {
+      console.log("Fetch Search List failed!", err.message);
+    }
+  };
+
+  // useEffect hook to fetch search listings when component mounts or search query changes
+  useEffect(() => {
+    getSearchListings(); // Calling getSearchListings function
+  }, [search]); // Dependency array includes search, so the effect runs when search changes
+
+  // JSX content to be rendered
+  return loading ? ( // If loading state is true, render Loader component
+    <Loader />
+  ) : ( // If loading state is false, render search results
+    <>
+      <Navbar /> {/* Rendering Navbar component */}
+      <h1 className="title-list">{search}</h1> {/* Title with search query */}
+      <div className="list">
+        {listings?.map(({ _id, creator, listingPhotoPaths, city, province, country, category, type, price, booking = false }) => (
+          <ListingCard // Rendering ListingCard component for each search result
+            key={_id} // Key prop for optimization and unique identification
+            listingId={_id} // Listing ID
+            creator={creator} // Creator ID
+            listingPhotoPaths={listingPhotoPaths} // Photo paths for listing
+            city={city} // City of listing
+            province={province} // Province of listing
+            country={country} // Country of listing
+            category={category} // Category of listing
+            type={type} // Type of listing
+            price={price} // Price of listing
+            booking={booking} // Booking status
+          />
+        ))}
+      </div>
+      <Footer /> {/* Rendering Footer component */}
+    </>
+  );
+};
+
+export default SearchPage; // Exporting SearchPage component
